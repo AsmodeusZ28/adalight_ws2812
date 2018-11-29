ce98b05d39f83940f72c5aeac1082dc82d9906d8
AsmodeusZ28 <zelda.man.3@gmail.com>
2018-11-28 18:41:23 -0600

    Changes to be committed: 	modified:   Adalight_WS2812.ino

--- Adalight_WS2812.ino
+++ Adalight_WS2812.ino
@@ -7,17 +7,23 @@
  * @date: 11/22/2015
  */
 #include "FastLED.h"
-#define NUM_LEDS 240
+#define NUM_LEDS 111
 #define DATA_PIN 6
+#define SENSOR_PIN A10
+
+//uint8_t valSensor = 255;
+fract8 smoothing = 255;
+bool updateReady = true;
 
 // Baudrate, higher rate allows faster refresh rate and more LEDs (defined in /etc/boblight.conf)
-#define serialRate 115200
+#define serialRate 256000
 
 // Adalight sends a "Magic Word" (defined in /etc/boblight.conf) before sending the pixel data
 uint8_t prefix[] = {'A', 'd', 'a'}, hi, lo, chk, i;
 
 // Initialise LED-array
 CRGB leds[NUM_LEDS];
+CRGB ledBuffer[NUM_LEDS];
 
 void setup() {
   // Use NEOPIXEL to keep true colors
@@ -40,7 +46,9 @@ void setup() {
 void loop() { 
   // Wait for first byte of Magic Word
   for(i = 0; i < sizeof prefix; ++i) {
-    waitLoop: while (!Serial.available()) ;;
+    waitLoop: //while (!Serial.available()) ;;
+    if (!Serial.available() ) goto updateArray;
+    
     // Check next byte in Magic Word
     if(prefix[i] == Serial.read()) continue;
     // otherwise, start over
@@ -61,8 +69,8 @@ void loop() {
     i=0;
     goto waitLoop;
   }
-  
-  memset(leds, 0, NUM_LEDS * sizeof(struct CRGB));
+
+//  memset(leds, 0, NUM_LEDS * sizeof(struct CRGB));
   // Read the transmission data and set LED values
   for (uint8_t i = 0; i < NUM_LEDS; i++) {
     byte r, g, b;    
@@ -72,11 +80,42 @@ void loop() {
     g = Serial.read();
     while(!Serial.available());
     b = Serial.read();
-    leds[i].r = r;
-    leds[i].g = g;
-    leds[i].b = b;
+    
+    ledBuffer[i].r = r;
+    ledBuffer[i].g = g;
+    ledBuffer[i].b = b;
+  }
+  updateReady = true;
+
+  updateArray:
+  if (updateReady) {
+    for (uint16_t i = 0; i < NUM_LEDS; i++) {
+      leds[i].r = lerp8by8( ledBuffer[i].r, leds[i].r, smoothing);
+      leds[i].g = lerp8by8( ledBuffer[i].g, leds[i].g, smoothing);
+      leds[i].b = lerp8by8(  ledBuffer[i].b,leds[i].b, smoothing);
+    }
+    FastLED.show();
+  }
+
+  EVERY_N_MILLISECONDS(100) {checkIn();}
+}
+
+void checkIn() {
+  updateReady = true;
+  float sensorValue = analogRead(SENSOR_PIN);
+
+  //most of the effect of smoothing can be seen in the higher values, with the lower values seeming very similar.
+  //So instead of linear input, I'm scaling it quadraticaly
+//  smoothing = ease8InOutQuad( map( analogRead(SENSOR_PIN), 0, 1023, 0, 240) );
+
+  if (sensorValue == 0) {
+    smoothing = 0;
+  } else {
+    smoothing = 0 - ( sensorValue / 1023 - 1 ) * ( sensorValue / 1023 - 1 ) *255 + 254;
   }
   
-  // Shows new values
-  FastLED.show();
+//  Serial.print("Sensor: ");
+//  Serial.println(sensorValue);
+//  Serial.print("Smoothing: ");
+//  Serial.println(smoothing);
 }
